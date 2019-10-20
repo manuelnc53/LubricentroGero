66b6ac4874f7c7eb4053e1ef22812a6482613bbb
luisr <luisr@DESKTOP-BIKADPF>
2019-10-20 09:59:31 -0300

    cree el metodo dame cliente que me trrae todos los clientes en la base de datos y me los devuelve en una lista

--- src/Model/ClienteDAO.java
+++ src/Model/ClienteDAO.java
@@ -5,12 +5,21 @@
  */
 package Model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author manuel
  */
 public class ClienteDAO implements DAO<ClienteModel> {
-    private Conexion conexion;
+    //private Conexion conexion;
+    private Connection conexion;
+    private PreparedStatement consulta;
     public ClienteDAO(){
     
     }
@@ -35,5 +44,35 @@ public class ClienteDAO implements DAO<ClienteModel> {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    public List dameClientes(){
+        List<ClienteModel> listaClientes = new ArrayList();//lista q contendra los clientes para la ventana de ventas
+        ResultSet resultado;
+        ClienteModel cliente= new ClienteModel();
+        try{
+            conexion=Conexion.getConnection();
+            consulta=conexion.prepareStatement("SELECT * FROM Clientes");
+            PreparedStatement consultaAux ;//tendra la consulta para traer los telefonos de los responsables
+            ResultSet resultadoAux;//tendra los telefonos de los responsables
+            List<String>telefonos= new ArrayList();
+            resultado=consulta.executeQuery();
+            String idAux;
+            while(resultado.next()){
+                cliente.setCuit_cuil(resultado.getInt(1));
+                cliente.setNombre(resultado.getString(2));
+                cliente.setDireccion(resultado.getString(3));
+                idAux=resultado.getString(1);
+                consultaAux = conexion.prepareStatement("SELECT * FROM ClientesTelefonosResponsable WHERE Clien_CUIT_CUIL_Tel ="+idAux);
+                resultadoAux=consultaAux.executeQuery();
+                while(resultadoAux.next()){
+                    telefonos.add(resultadoAux.getString(2));
+                }
+                cliente.setTelefonos_Del_Responsable((ArrayList<String>) telefonos);//agrego los telefonos al cliente
+                listaClientes.add(cliente.clone());
+            }
+        }catch(SQLException e){
+            System.out.println("No se pudo realizar la consulta");
+        }
+    return listaClientes;    
+    }
     
 }
